23def22ddd39f536fad3296b997206e7a568983b
Bhavesh Sakpal <iambhaveshsakpal@gmail.com>
2023-12-27 18:04:03 +0530

    added env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TaskController } from './task/task.controller';
 import { TaskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskSchema } from './schemas/task.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TaskModule,
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://ibhaveshsakpal:bhavesh@cluster0.xdmpkvy.mongodb.net/?retryWrites=true&w=majority',
+      process?.env?.MONGO_URL,
     ),
     MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }]),
   ],
